0b6eb4346ba660b3ae9abe87344b5f40b0229f0e
sajicode <sajioloye@gmail.com>
2018-12-02 20:03:55 +0100

    separate routes from network and prepare app for deployment

--- app.js
+++ app.js
@@ -5,14 +5,11 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const cons = require('consolidate');
 const dust = require('dustjs-helpers');
-const {
-  Client
-} = require('pg');
+
+const router = require('./routes');
 
 const app = express();
 
-// DB connect string
-const connectionString = process.env.CONNECT;
 const PORT = process.env.PORT;
 
 // Assign dust engine to .dust files
@@ -33,74 +30,8 @@ app.use(
   })
 );
 
-app.get('/', function (req, res) {
-
-  const client = new Client({
-    connectionString: connectionString
-  });
-  client.connect();
-
-  client.query('SELECT * FROM recipes', (err, result) => {
-    if (err) {
-      return console.error('Client error', err);
-    } else {
-      // console.log('Client response', result.rows);
-      res.render('index', {
-        recipes: result.rows
-      });
-      client.end();
-    }
-  });
-});
-
-app.post('/add', function (req, res) {
-  const client = new Client({
-    connectionString: connectionString
-  });
-  client.connect();
-
-  client.query('INSERT INTO recipes(name, ingredients, directions) VALUES($1, $2, $3)', [
-    req.body.name,
-    req.body.ingredients,
-    req.body.directions
-  ]);
-
-  res.redirect('/');
-  // done();
-
-});
-
-app.delete('/delete/:id', function (req, res) {
-  const client = new Client({
-    connectionString: connectionString
-  });
-  client.connect();
-
-  client.query('DELETE FROM recipes WHERE id = $1', [
-    req.params.id
-  ]);
-
-  // done();
-  res.sendStatus(200)
-  // res.redirect('/');
-});
-
-app.post('/edit', function (req, res) {
-  const client = new Client({
-    connectionString: connectionString
-  });
-  client.connect();
-
-  client.query('UPDATE recipes SET name=$1, ingredients=$2, directions=$3 WHERE id=$4', [
-    req.body.name,
-    req.body.ingredients,
-    req.body.directions,
-    req.body.id
-  ]);
-
-  // done();
-  res.redirect('/');
-});
+// connect router
+app.use(router);
 
 // server
 app.listen(PORT, function () {
---
+++ routes.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const {
+  Client
+} = require('pg');
+
+// DB connect string
+const connectionString = process.env.CONNECT;
+
+const router = express.Router();
+
+router.get('/', function (req, res) {
+
+  const client = new Client({
+    connectionString: connectionString
+  });
+  client.connect();
+
+  client.query('SELECT * FROM recipes', (err, result) => {
+    if (err) {
+      return console.error('Client error', err);
+    } else {
+      // console.log('Client response', result.rows);
+      res.render('index', {
+        recipes: result.rows
+      });
+      client.end();
+    }
+  });
+});
+
+router.post('/add', function (req, res) {
+  const client = new Client({
+    connectionString: connectionString
+  });
+  client.connect();
+
+  client.query('INSERT INTO recipes(name, ingredients, directions) VALUES($1, $2, $3)', [
+    req.body.name,
+    req.body.ingredients,
+    req.body.directions
+  ]);
+
+  res.redirect('/');
+  // done();
+
+});
+
+router.delete('/delete/:id', function (req, res) {
+  const client = new Client({
+    connectionString: connectionString
+  });
+  client.connect();
+
+  client.query('DELETE FROM recipes WHERE id = $1', [
+    req.params.id
+  ]);
+
+  // done();
+  res.sendStatus(200)
+  // res.redirect('/');
+});
+
+router.post('/edit', function (req, res) {
+  const client = new Client({
+    connectionString: connectionString
+  });
+  client.connect();
+
+  client.query('UPDATE recipes SET name=$1, ingredients=$2, directions=$3 WHERE id=$4', [
+    req.body.name,
+    req.body.ingredients,
+    req.body.directions,
+    req.body.id
+  ]);
+
+  // done();
+  res.redirect('/');
+});
+
+module.exports = router;
